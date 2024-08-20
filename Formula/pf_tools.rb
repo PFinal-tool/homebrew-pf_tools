c20ed28eb709bb55ee6f6d429f739b9039a102a2
PFinal_南丞 <1370288056@qq.com>
2024-08-20 16:52:15 +0800

    更新增加 arm的支持

--- Formula/pf_tools.rb
+++ Formula/pf_tools.rb
@@ -6,6 +6,11 @@ class PfTools < Formula
   if Hardware::CPU.intel?
     url "https://github.com/PFinal-tool/pf_tools/releases/download/v1.0.1/pf_tools-darwin_amd64"
     sha256 "d58b2b0b4d9efb6f7e86f6816d1b7d86a3b64db630012a3a1e5f6088ccd1dc7c"
+  elsif Hardware::CPU.arm?
+    url "https://github.com/PFinal-tool/pf_tools/releases/download/v1.0.1/pf_tools-darwin_arm64"
+    sha256 "1fd49c1c3a518ecbb1ca4a2e48961461acc3b67958e188b76031523f437fbb58"
+  else
+    odie "Unsupported CPU architecture"
   end
 
   def install
